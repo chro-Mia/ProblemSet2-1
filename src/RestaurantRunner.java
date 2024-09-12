a1bf217a6e70ad47a7df4f6e6ea24ef017a3612f
chro-Mia <tessa.chromia@gmail.com>
2024-09-12 12:20:49 -0700

    comments | prod ready

--- src/RestaurantRunner.java
+++ src/RestaurantRunner.java
@@ -3,19 +3,26 @@ public class RestaurantRunner
 {
     public static void main (String[] args){
 
+        //create menu object
         Menu menu = new Menu("Dollar Burger");
 
+        //add menu items
         menu.addItem("Burger", 1);
         menu.addItem("Fries", 1);
         menu.addItem("Drink", 1);
 
+        //print out menu
         System.out.println(menu);
 
         Scanner input = new Scanner(System.in);
 
+        //hilarious variable name
         boolean willThatBeAll = false;
         Order order = new Order(menu);
 
+        //while the customer is still ordering
+        //take their input and add their items to the order
+        //if they're done ordering, calculate their total and ask if they have enough
         while(!willThatBeAll) {
             System.out.println("What would you like to order?: ");
             int index = input.nextInt();
--- src/StringRemover.java
+++ src/StringRemover.java
@@ -4,15 +4,31 @@ public class StringRemover
     public static void main (String[] args){
 
         int pointer;
+        String string2;
         Scanner input = new Scanner(System.in);
 
         System.out.print("Enter a string: ");
         String string1 = input.nextLine();
         System.out.println("Enter a shorter string to remove from the first string: ");
-        String string2 = input.nextLine();
+        string2 = input.nextLine();
 
+        //"error" checking loop, also i think a do/while loop would work well here
         //blah blah blah shut up, mr kraut wants me to use indexOf()
-        while(string1.indexOf(string2) > -1){
+        while(string1.indexOf(string2) == -1){
+            System.out.println("That substring wasn't found in the first string!" +
+                    "\nEnter a shorter string to remove from the first string: ");
+            string2 = input.nextLine();
+        }
+
+        /*
+        step 1: print the substring containing the characters from index 0 to the index of string2
+        step 2: set the pointer to the index of the character after string2
+        step 3: remove a substring of string1 that contains the characters from index 0 to the index of the last
+                character of string2
+        step 4: if no string2 is found within string1, print the rest of string1 and exit the loop
+        step 5: repeat
+        */
+        while(true){
 
             System.out.print(string1.substring(0, string1.indexOf(string2)));
             pointer = string1.indexOf(string2) + string2.length();

080bc08319704cdf03fb6bd6da77eaef983c40d1
chro-Mia <tessa.chromia@gmail.com>
2024-09-17 08:36:21 -0700

    update | complete

--- src/RestaurantRunner.java
+++ src/RestaurantRunner.java
@@ -3,33 +3,35 @@ public class RestaurantRunner
 {
     public static void main (String[] args){
 
-        //create menu object
-        Menu menu = new Menu("Dollar Burger");
-
-        //add menu items
-        menu.addItem("Burger", 1);
-        menu.addItem("Fries", 1);
-        menu.addItem("Drink", 1);
-
-        //print out menu
-        System.out.println(menu);
-
-        Scanner input = new Scanner(System.in);
-
         //hilarious variable name
         boolean willThatBeAll = false;
 
         //string for formatting
         String other = "";
 
-        //create new order
-        Order order = new Order(menu);
+        //create menu object
+        Menu menu = new Menu("Breakfast");
+        menu.addItem("Pancakes", 1);
+        menu.addItem("Waffles", 1);
+        menu.addItem("Orange Juice", 1);
 
-        //while the customer is still ordering
-        //take their input and add their items to the order
-        //if they're done ordering, calculate their total and ask if they have enough
-        //also i cant tell if i should edit the order class or just add the extra logic here?
-        while(!willThatBeAll) {
+        Menu secondMenu = new Menu("Not Breakfast");
+        secondMenu.addItem("Burger", 1);
+        secondMenu.addItem("Fries", 1);
+        secondMenu.addItem("Drink", 1);
+
+        //print out menu
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to Dollar Burger");
+        System.out.println(menu);
+        System.out.println(secondMenu);
+        System.out.println("Would you like or order from the first or second menu?");
+        int menuChoice = input.nextInt();
+
+        while(!willThatBeAll && menuChoice == 1) {
+
+            Order order = new Order(menu);
+
             System.out.println("What " + other + "would you like to order?: ");
             int index = input.nextInt();
 
@@ -41,6 +43,42 @@ public class RestaurantRunner
 
             System.out.println("Will that be all?: ");
             willThatBeAll = input.nextBoolean();
+
+            if(willThatBeAll){
+                //why
+                String orderList = order.toString();
+                System.out.println("You ordered:\n" + orderList);
+                System.out.println("Your total is: " + order.getTotal());
+
+                System.out.println("How much money do you have?");
+                double money = input.nextDouble();
+
+                if(money >= order.getTotal()){
+                    System.out.println("Thank you for your purchase!");
+                    System.out.println("Your change is " + (money - order.getTotal()) + " dollars.");
+                }
+                else{
+                    System.out.println("You don't have enough money.");
+                }
+            }
+        }
+
+        while(!willThatBeAll && menuChoice == 2) {
+
+            Order order = new Order(secondMenu);
+
+            System.out.println("What " + other + "would you like to order?: ");
+            int index = input.nextInt();
+
+            System.out.println("How many do you want?: ");
+            int amount = input.nextInt();
+
+            order.add(index, amount);
+            other = "else ";
+
+            System.out.println("Will that be all?: ");
+            willThatBeAll = input.nextBoolean();
+
             if(willThatBeAll){
                 //why
                 String orderList = order.toString();
